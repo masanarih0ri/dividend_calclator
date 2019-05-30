ff460eb20bdb4ef56f5e2fa56e26f627431f6cf0
m-hori <m-hori@m3career.com>
2019-05-30 11:58:09 +0900

    vuetifyでtableとheader,footerを追加

--- dividend_calclator/src/App.vue
+++ dividend_calclator/src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="app">
+  <v-app id="app">
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>配当金計算アプリ</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <!-- router-viewによって、それぞれのパスごとのviewを表示する -->
     <router-view/>
-  </div>
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }} Masanari Hori</div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -17,6 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- dividend_calclator/src/components/AssetManagementCalculate.vue
+++ dividend_calclator/src/components/AssetManagementCalculate.vue
@@ -1,49 +1,34 @@
+<!--
+・とりあえず、最低限のアプリケーションはできたので、綺麗にして公開したい
+・Vuetifyでform部分とtableを作り込みたい
+・複数のコンポーネントを共存させることができるか？
+・グラフの描画（イメージは
+
+ -->
 <template>
   <div class="calculate_section">
-    <h1>配当金計算アプリ</h1>
-
-    <v-container
-    id="input-usage"
-    grid-list-xl
-    fluid
-  >
-    <v-layout wrap>
-      <v-flex xs12>
-        <v-input
-          :messages="['Messages']"
-          append-icon="close"
-          prepend-icon="phone"
-        >
-          Default Slot
-        </v-input>
-      </v-flex>
-    </v-layout>
-  </v-container>
-
-
-
-    <div class="">
+    <div class="calculate_section__input_box">
       <label for="stock_price">現在株価</label>
       <!-- .numberがなければ文字列として認識してしまう -->
       <input class="" type="number" v-model.number="stock_price" placeholder="現在の株価を入力してください">円
     </div>
-    <div class="">
+    <div class="calculate_section__input_box">
       <label for="earnings_per_share">1株あたり配当金</label>
       <input type="number" v-model.number="earnings_per_share" placeholder="1株あたり配当金を入力してください">円
     </div>
     <p>年間配当利回り:{{ dividendYieldCalculate }} %</p>
-    <div class="">
+    <div class="calculate_section__input_box">
       <label for="number_of_shares">購入株数</label>
       <input type="number" v-model.number="number_of_shares" placeholder="購入したい株数を入力してください">株
     </div>
-    <div class="">
+    <div class="calculate_section__input_box">
       <label for="number_of_years">運用する年数</label>
       <input type="number" v-model.number="number_of_years" placeholder="運用する年数を入力してください">年
     </div>
     <div class="">
       <p>{{ number_of_years }}年後には、{{ amountOfShare }}円が{{ totalAmountOfMoney }}円となります。</p>
       <p>ここから下に利回り計算後の表が出ます。</p>
-      <table>
+      <!-- <table>
         <tr>
           <th></th>
           <td>利回り計算後の金額</td>
@@ -52,12 +37,128 @@
           <th>{{ number_of_year }}年後</th>
           <td>{{ Math.round(amountOfShare * Math.pow(rateOfIncrease, number_of_year)) }}円</td>
         </tr>
-      </table>
+      </table> -->
+      <!--
+      参考：https://vuetifyjs.com/ja/components/data-tables
+      -->
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        class="elevation-1"
+      >
+        <template v-slot:items="props">
+          <tr v-for="number_of_year in number_of_years">
+            <th>{{ number_of_year }}年後</th>
+            <td>{{ Math.round(amountOfShare * Math.pow(rateOfIncrease, number_of_year)) }}円</td>
+          </tr>
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script>
+// export default {
+//     data () {
+//       return {
+//         headers: [
+//           {
+//             text: 'Dessert (100g serving)',
+//             align: 'left',
+//             sortable: false,
+//             value: 'name'
+//           },
+//           { text: 'Calories', value: 'calories' },
+//           { text: 'Fat (g)', value: 'fat' },
+//           { text: 'Carbs (g)', value: 'carbs' },
+//           { text: 'Protein (g)', value: 'protein' },
+//           { text: 'Iron (%)', value: 'iron' }
+//         ],
+//         desserts: [
+//           {
+//             name: 'Frozen Yogurt',
+//             calories: 159,
+//             fat: 6.0,
+//             carbs: 24,
+//             protein: 4.0,
+//             iron: '1%'
+//           },
+//           {
+//             name: 'Ice cream sandwich',
+//             calories: 237,
+//             fat: 9.0,
+//             carbs: 37,
+//             protein: 4.3,
+//             iron: '1%'
+//           },
+//           {
+//             name: 'Eclair',
+//             calories: 262,
+//             fat: 16.0,
+//             carbs: 23,
+//             protein: 6.0,
+//             iron: '7%'
+//           },
+//           {
+//             name: 'Cupcake',
+//             calories: 305,
+//             fat: 3.7,
+//             carbs: 67,
+//             protein: 4.3,
+//             iron: '8%'
+//           },
+//           {
+//             name: 'Gingerbread',
+//             calories: 356,
+//             fat: 16.0,
+//             carbs: 49,
+//             protein: 3.9,
+//             iron: '16%'
+//           },
+//           {
+//             name: 'Jelly bean',
+//             calories: 375,
+//             fat: 0.0,
+//             carbs: 94,
+//             protein: 0.0,
+//             iron: '0%'
+//           },
+//           {
+//             name: 'Lollipop',
+//             calories: 392,
+//             fat: 0.2,
+//             carbs: 98,
+//             protein: 0,
+//             iron: '2%'
+//           },
+//           {
+//             name: 'Honeycomb',
+//             calories: 408,
+//             fat: 3.2,
+//             carbs: 87,
+//             protein: 6.5,
+//             iron: '45%'
+//           },
+//           {
+//             name: 'Donut',
+//             calories: 452,
+//             fat: 25.0,
+//             carbs: 51,
+//             protein: 4.9,
+//             iron: '22%'
+//           },
+//           {
+//             name: 'KitKat',
+//             calories: 518,
+//             fat: 26.0,
+//             carbs: 65,
+//             protein: 7,
+//             iron: '6%'
+//           }
+//         ]
+//       }
+//     }
+//   }
 export default {
   name: 'AssetManagementCalculate',
   data () {
@@ -65,7 +166,27 @@ export default {
       stock_price: null,
       earnings_per_share: null,
       number_of_shares: null,
-      number_of_years: null
+      number_of_years: null,
+      headers: [
+        {
+          text: '経過年数',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: '利回り計算後の金額',
+          value: 'calories',
+          sortable: false
+        }
+      ],
+      desserts: [
+        {
+          name: 'Donut',
+          calories: 518,
+        }
+      ]
+
     }
   },
 
@@ -108,4 +229,8 @@ li {
 a {
   color: #42b983;
 }
+
+/* .v-datatable__actions {
+  display: none !important;
+} */
 </style>
